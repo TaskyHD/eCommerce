2419d85d2a8f585728cd77dc8f4cd7175d866926
TaskyHD <tasky.hd@icloud.com>
2017-05-02 21:58:35 +0200

    ora va direttamente a prodotti (lalilulelo)

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect('prodotti/')),
     url(r'^admin/', admin.site.urls),
     url(r'^prodotti/', include('prodotti.urls'))
 ]
