68948e0a02218a9763a288ebc0dcd6ccfa2327d1
Alessio Popoli <apopoli95@gmail.com>
2017-05-03 21:31:58 +0200

    Modificata pagina principale con saliera

--- prodotti/views.py
+++ prodotti/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,reverse
 from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.template import loader
+from .models import Prodotto
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.")
+    template = loader.get_template('prodotti/lista.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
 
 def listaprod(request):
     context={}
