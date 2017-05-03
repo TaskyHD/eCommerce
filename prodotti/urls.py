add20d921dbb72aec33ba0af5f8d141b781323cf
TaskyHD <tasky.hd@icloud.com>
2017-05-03 21:30:54 +0200

    urls per il debugs (El Psy Congroo)

--- prodotti/urls.py
+++ prodotti/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
+    ##CAVALO È PER PROVARE LE COSE
+    url(r'^$', lambda r: HttpResponseRedirect('lista/')),
+    ##AMMAZZAMI MALE ↑
     url(r'lista',views.listaprod,name='listaprodotti')
 ]
