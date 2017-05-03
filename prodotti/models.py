a6520fb524f130ec6ecefcb705dd3a9fe9792515
TaskyHD <tasky.hd@icloud.com>
2017-05-03 21:21:43 +0200

    messo una saliera in vendita (lalilulelo)

--- prodotti/models.py
+++ prodotti/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Prodotto:
     def __init__(self, imgUrl, nome, descr, prezzo):
+
         __imgUrl = imgUrl
         __nome = nome
         __descr = descr
--- prodotti/views.py
+++ prodotti/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,reverse
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world.")
+
+def listaprod(request):
+    context={}
+    return render(request,"prodotti/lista.html",context)
