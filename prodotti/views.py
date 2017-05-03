3c5dd22d65e005ac2cfdff162edc21cc2d331035
TaskyHD <tasky.hd@icloud.com>
2017-05-03 21:55:04 +0200

    descr (El Psy Congroo)

--- prodotti/views.py
+++ prodotti/views.py
@@ -13,3 +13,7 @@ def listaprod(request):
     prod=Prodotto.objects.get(id=1);
     context={"nome":prod.nome,"prezzo":prod.prezzo,"img":prod.imgUrl}
     return render(request,"prodotti/lista.html",context)
+def prodsing(request,id):
+    prod = Prodotto.objects.get(id=1);
+    context = {"nome": prod.nome, "prezzo": prod.prezzo, "img": prod.imgUrl,"descr":prod.descr}
+    return render(request, "prodotti/lista.html", context)
