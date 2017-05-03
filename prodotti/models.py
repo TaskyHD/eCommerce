69e751942fe2982138b5151d76ca080a787f6fc5
Alessio Popoli <apopoli95@gmail.com>
2017-05-03 21:20:26 +0200

    Aggiunta tabella prodotti al database

--- prodotti/models.py
+++ prodotti/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Prodotto:
-    def __init__(self, imgUrl, nome, descr, prezzo):
-        __imgUrl = imgUrl
-        __nome = nome
-        __descr = descr
-        __prezzo = prezzo
+class Prodotto(models.Model):
+    imgUrl = models.CharField(max_length=200)
+    nome = models.CharField(max_length=100)
+    descr = models.TextField(max_length=1000)
+    prezzo = models.DecimalField(decimal_places=2, max_digits=20)
+
+    def __str__(self):
+        return self.nome
